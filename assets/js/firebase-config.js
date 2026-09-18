dbe98a2aa70bd8df0a3bee70f81f41ed948551e9
fwzanmahr046-lab <fwzanmahr046@gmail.com>
2026-09-18 10:42:02 +0300

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,19 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// ⚠️ استبدل هذه القيم بمفاتيح مشروعك من Firebase Console
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyALtwlWE7LMM-AY2_7OWr4Tuw_bpO_y1v8",
+  authDomain: "poper-c38ba.firebaseapp.com",
+  projectId: "poper-c38ba",
+  storageBucket: "poper-c38ba.firebasestorage.app",
+  messagingSenderId: "107824321178",
+  appId: "1:107824321178:web:b2a7ab5d494fa393894523"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app);
